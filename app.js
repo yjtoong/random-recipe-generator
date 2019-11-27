6493096c5d63ed6b8d143253752eafeae5c95550
yjtoong <41092243+yjtoong@users.noreply.github.com>
2019-11-27 08:57:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // heroku
 if (process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
-  app.get('*', (req,res) => {
+  app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
